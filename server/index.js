18715904143480d6aeb4bef753bf8a412e491dcf
sanvika15 <137372400+sanvika15@users.noreply.github.com>
2024-06-24 14:23:18 +0530

    route in index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
 
 // database connection
 connection();
@@ -10,6 +12,10 @@ connection();
 // middlewares
 app.use(express.json());
 app.use(cors());
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
